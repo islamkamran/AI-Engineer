f8fd01a098ac6cbbf98bfbefde7a3a78e4eda239
MUHAMMAD ISLAM KAMRAN <islamkamran332@gmail.com>
2025-10-18 13:04:07 +0400

    AI Learning python dict and tuples

--- dict_tuple.py
+++ dict_tuple.py
@@ -11,5 +11,19 @@ print("---------- Print by Tuple ---------")
 for k,v in d.items():
     print(f'key: {k}, Value:{v}')
 
-# checking the python condition without checking thr if condition
+# checking the python condition true or false
 print("islam" in d)
+
+
+d.clear()
+print(d)
+
+point = (23,72)
+x = point[0]
+print(x)
+# checking error condtion run time assignment
+# point[0]=44
+# print(x)
+point=(44,72)
+x = point[0]
+print(x)
